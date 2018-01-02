9651b3d71597aa70d90bb85c106f560c87bad22b
Matthias Kalb <matthias.kalb@railsmechanic.de>
2018-01-02 15:58:22 +0100

    Updates to docs

--- lib/nanoid.ex
+++ lib/nanoid.ex
@@ -3,9 +3,11 @@ defmodule Nanoid do
   Elixir port of the NanoID ([https://github.com/ai/nanoid](https://github.com/ai/nanoid)) ID generator.
   A tiny, secure URL-friendly unique string ID generator.
 
-  *Safe.* It uses cryptographically strong random APIs and guarantees a proper distribution of symbols.
-  *Small.* Only 179 bytes (minified and gzipped). No dependencies. It uses Size Limit to control size.
-  *Compact.* It uses a larger alphabet than UUID (A-Za-z0-9_~) and has a similar number of unique IDs in just 21 symbols instead of 36.
+  **Safe.** It uses cryptographically strong random APIs and guarantees a proper distribution of symbols.
+
+  **Small.** Only 179 bytes (minified and gzipped). No dependencies. It uses Size Limit to control size.
+
+  **Compact.** It uses a larger alphabet than UUID (A-Za-z0-9_~) and has a similar number of unique IDs in just 21 symbols instead of 36.
   """
   use Bitwise
 
