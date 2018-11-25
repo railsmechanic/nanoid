b0b76cd751618b7bf1bfb944c7eb0d68f718d15a
Matthias Kalb <matthias.kalb@railsmechanic.de>
2018-11-25 16:38:25 +0100

    Changed version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nanoid.Mixfile do
   def project do
     [
       app: :nanoid,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       description: description(),
